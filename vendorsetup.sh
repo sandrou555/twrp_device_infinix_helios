7d6b31694d9414630461bbc9920f00660577cc50
TegarXLu <136581622+sandrou555@users.noreply.github.com>
2025-03-05 22:09:16 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,7 +18,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export TW_DEFAULT_LANGUAGE="en"
     export OF_DEVICE_ALT="helios,FULL-64"
 	export LC_ALL="C"
-	export OF_AB_DEVICE=1
+	export FOX_AB_DEVICE=1
 	export FOX_EXTREME_SIZE_REDUCTION=1
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export OF_QUICK_BACKUP_LIST="/data;"
